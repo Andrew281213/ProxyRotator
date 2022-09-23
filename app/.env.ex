4358a66ea1f24ea3a52aa8f8bcdbe801a9bb3dde
Andrew281213 <andrey.domakhin8@ya.ru>
2022-09-24 02:14:54 +0300

    Добавлено уведомление в тг о старте, описание, изменены ссылки на доки

--- app/.env.ex
+++ app/.env.ex
@@ -2,3 +2,7 @@
 PROXY_TYPE_SEPARATOR=;
 # Путь к файлу со сслыками на прокси
 URLS_FILEPATH=path_to_file_with_urls
+# Токен тг бота
+TG_BOT_TOKEN=1111:5742685768234765
+# Id тг чата
+TG_CHAT_ID=111111
--- app/api.py
+++ app/api.py
@@ -1,22 +1,73 @@
+import os
+
 import uvicorn
+import requests
 
 from fastapi import FastAPI
 
-from rotator import ProxyRotator
+from rotator import ProxyRotator, logger
 
 
-app = FastAPI()
+description = """
+Простое api для получения прокси
+
+TG: @tanf93
+"""
+
+
+app = FastAPI(
+	title="Простое api для ротации прокси",
+	description=description,
+	version="1.0.0",
+	contact={
+		"name": "Андрей Домахин",
+		"email": "andrey.domakhin8@ya.ru",
+	},
+	openapi_url="/help/openapi.json",
+	redoc_url="/help/redoc",
+	docs_url="/help/docs"
+)
 app.state.rotator = ProxyRotator()
 
 
-@app.get("/proxy")
-async def get_proxy_cnt(cnt: int = 1):
+def send_tg_msg(msg):
+	"""Отправляет сообщение в тг бот
+
+	:param str msg: Текст сообщения
+	"""
+	tg_chat_id = os.environ.get("TG_CHAT_ID")
+	tg_bot_token = os.environ.get("TG_BOT_TOKEN")
+	logger.debug(f"Отправляю сообщение в тг бот пользователю {tg_chat_id}")
+	url = f"https://api.telegram.org/bot{tg_bot_token}/sendmessage"
+	if tg_chat_id is None or tg_bot_token is None:
+		logger.warning("Id пользователя и токен бота не должны быть пустыми")
+		return
+	params = {
+		"chat_id": tg_chat_id,
+		"text": msg,
+		"parse_mode": "HTML",
+		"disable_web_page_preview": "true"
+	}
+	try:
+		r = requests.get(url, params=params)
+		r.raise_for_status()
+		if r.status_code == 200:
+			logger.debug(f"Сообщение \"{msg}\" в тг успешно отправлено")
+	except Exception as e:
+		logger.warning(f"Не удалось отправить сообщение в тг \"{msg}\", {e}")
+
+
+send_tg_msg("Сервис <b>Proxy Rotator</b> успешно запущен")
+
+
+@app.get("/proxy", status_code=200, tags=["proxy"])
+async def get_proxy_cnt(cnt: int = 1) -> str or dict:
 	"""Получает cnt случайных прокси"""
 	proxy_list = [app.state.rotator.sync_get_proxy() for _ in range(cnt)]
 	return proxy_list[0] if cnt == 1 else proxy_list
 
 
-@app.get("/proxies")
+@app.get("/proxies", status_code=200, tags=["proxy", "proxies"])
 async def proxies():
 	"""Выводит все доступные прокси"""
 	return {
